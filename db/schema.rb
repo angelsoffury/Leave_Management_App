71e12bf7a0cbdd4fb9a2b939d80e4f7e3d22946a
angelsoffury <ahalyadas01@gmail.com>
2015-07-08 01:27:28 +0530

    Make a basic User model (including secure passwords)

--- db/schema.rb
+++ db/schema.rb
@@ -11,31 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150705201500) do
-
-  create_table "leaves", force: true do |t|
-    t.date     "start_date"
-    t.date     "end_date"
-    t.text     "reason"
-    t.integer  "user_id"
-    t.integer  "approver_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "status",      default: "Pending"
-  end
-
-  add_index "leaves", ["user_id", "created_at"], name: "index_leaves_on_user_id_and_created_at"
-
-  create_table "roles", force: true do |t|
-    t.integer  "manager_id"
-    t.integer  "employee_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "roles", ["employee_id"], name: "index_roles_on_employee_id"
-  add_index "roles", ["manager_id", "employee_id"], name: "index_roles_on_manager_id_and_employee_id", unique: true
-  add_index "roles", ["manager_id"], name: "index_roles_on_manager_id"
+ActiveRecord::Schema.define(version: 20150707194852) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -44,11 +20,6 @@ ActiveRecord::Schema.define(version: 20150705201500) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
-    t.string   "remember_token"
-    t.boolean  "admin",           default: false
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
