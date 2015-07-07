12a72261380c79f3d4142ae3ce9405c5158144ce
angelsoffury <ahalyadas01@gmail.com>
2015-07-08 02:32:18 +0530

    Finish user edit and update  actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+   before_action :signed_in_user, only: [:edit, :update]
+   before_action :correct_user,   only: [:edit, :update]
 
    def show
     @user = User.find(params[:id])
@@ -12,12 +14,26 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to the Leave Management App!"
       redirect_to @user
     else
       render 'new'
     end
   end
+
+  def edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   
   private
 
@@ -25,5 +41,18 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+    # Before filters
+
     
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
